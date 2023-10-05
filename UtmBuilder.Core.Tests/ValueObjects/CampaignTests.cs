c094c5e273ad487e7af94c49b35ee66dbefceb02
Dgs Abdo <douglas121996@hotmail.com>
2023-10-05 11:11:11 -0300

    test: adicionando teste de Campaign

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utmbuilder.Core.ValueObjects;
+using Utmbuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class CampaignTests
+    {
+        [TestMethod]
+        [DataRow("", "", "", true)]
+        [DataRow("src", "", "", true)]
+        [DataRow("src", "med", "", true)]
+        [DataRow("src", "med", "nme", false)]
+        public void CampaignTest(string source, string medium, string name, bool expectException) 
+        {
+            if(expectException)
+            {
+                try
+                {
+                    new Campaing(source, medium, name);
+                    Assert.Fail();
+                }
+                catch (InvalidCampaignException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Campaing(source, medium, name);
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
